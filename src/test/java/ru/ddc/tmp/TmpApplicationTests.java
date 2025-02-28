2519068f98b8f7fe58a7a6eccae2cdbfb2683661
svkovalev <svkovalev@bank-hlynov.ru>
2025-02-28 10:49:04 +0300

    feat :: added project template

---
+++ src/test/java/ru/ddc/tmp/TmpApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ddc.tmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
